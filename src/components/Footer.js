120cead5ae51d72042c8639df1cd58a78f4f8367
aram1l7 <arammartirosyann16@gmail.com>
2023-04-15 01:50:22 +0400

    Fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base ">
       <Layout className="py-8 flex justify-between items-center xl:!p-12 md:!p-4 xs:flex-col xs:gap-3">
-        <span>Copyright &copy; {new Date().getFullYear()} By Aram.</span>
+        <span className="select-none">
+          &copy; {new Date().getFullYear()} by
+          &#x1D49C;&#x1D4C7;&#x1D4B6;&#x1D4C2; &#x2665;
+        </span>
         <span>
           <a href="mailto:aram18m@gmail.com">aram18m@gmail.com</a>
         </span>
