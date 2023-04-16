8053b6da5c9ea161a8697050def0258a69f7142a
aram1l7 <arammartirosyann16@gmail.com>
2023-04-16 20:23:24 +0400

    Experience in progress

---
+++ src/mocks/job-history.js
@@ -0,0 +1,40 @@
+export const jobHistory = [
+  {
+    id: 1,
+    position: "Frontend Developer",
+    company: "Evistep Enterprise",
+    time: "01/2021 - 07/2021",
+    location: "Yerevan, Armenia",
+    type: "Full time",
+    description: `Developed company's biggest product's landing page, and implemented various UX features for the products dashboard`,
+  },
+  {
+    id: 2,
+    position: "Javascript Developer",
+    company: "Blowsys",
+    time: "06/2022 - 10/2022",
+    type: "Contract",
+    location: "Yerevan, Armenia",
+    description: `Designed and developed companys landing page which later won an award, implemented full stack mailing feature for users to apply for jobs with a file attachment option`,
+  },
+  {
+    id: 3,
+    position: "React Developer",
+    company: "Cretrix",
+    time: "07/2021 - 11/2022",
+    type: "Full time",
+    location: "Yerevan, Armenia",
+    description:
+      "Worked on a social media app with a great team of designers and developers as a frontend developer, improved the apps dashboard and building new ux features for 6 months.Later developed a blockchain landing page with blog posts from scratch with React/Node",
+  },
+  {
+    id: 4,
+    position: "Full Stack Develoepr",
+    company: "Itsoft",
+    time: "11/2022 - Present",
+    type: "Contract",
+    location: "Remote",
+    description:
+      "Working on a big data analytics app as a full stack developer, implementing user authentication with JWT, building new frontend and backend features such as CRUD,spam management, mailing, reset password, etc.",
+  },
+];
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import Layout from "@/components/Layout";
+import React from "react";
+import { jobHistory } from "@/mocks/job-history";
+
+const Details = ({ position, company, time, type, description, location }) => {
+  return (
+    <li>
+      <div>
+        <h3>
+          {position} @{company}
+        </h3>
+        <span>
+          {time} | {location} | {type}
+        </span>
+        <p>{description}</p>
+      </div>
+    </li>
+  );
+};
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <main className="flex w-full flex-col items-center justify-center dark:text-light">
+      <Layout className="pt-16">
+        <h1
+          className="inline-block text-dark dark:text-light
+      font-bold w-full !text-6xl xl:!text-5xl xl:text-center
+      lg:!text-6xl md:!text-5xl sm:!text-3xl animate-fade sm:mb-8 mb-16"
+        >
+          My journey
+        </h1>
+        <div className="relative w-[75%] mx-auto lg:w-[90%] md:w-full mt-16">
+          {jobHistory.map((el) => {
+            return <Details key={el.id} {...el} />;
+          })}
+        </div>
+      </Layout>
+    </main>
+  );
+};
 
-export default Experience
+export default Experience;
