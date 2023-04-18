5a4b1e289f3b0adea823f26d5905c0af63d69844
aram1l7 <arammartirosyann16@gmail.com>
2023-04-19 01:19:38 +0400

    In progress

--- src/mocks/projects.js
+++ src/mocks/projects.js
@@ -1,32 +1,38 @@
 export const projects = [
   {
     id: 1,
-    title: "Blowsys LLC landing page",
+    title: "Blowsys",
     link: "https://blowsys.com/",
+    img: "/images/works/blowsys.png",
   },
   {
     id: 2,
-    title: "SustainDAO blog",
+    title: "SustainDAO",
     link: "https://sustaindao.xyz/",
+    img: "/images/works/sustaindao.png",
   },
   {
     id: 3,
     title: "Artist Portfolio",
     link: "https://artist-portfolio-web.vercel.app/",
+    img: "/images/works/artist-portfolio.png",
   },
   {
     id: 4,
-    title: "Ringplan webphone",
+    title: "Ringplan",
     link: "https://webphone.dev.ringplan.com/",
+    img: "/images/works/ringplan.png",
   },
   {
     id: 5,
-    title: "Cretrix LLC landing page",
+    title: "Cretrix",
     link: "https://cretrix.github.io/",
+    img: "/images/works/cretrix.png",
   },
   {
     id: 6,
-    title: "Contact form",
+    title: "Mailer app",
     link: "https://mui-next-form.vercel.app/",
+    img: "/images/works/contact-form.png",
   },
 ];
--- src/pages/work.js
+++ src/pages/work.js
@@ -9,16 +9,38 @@ const Work = () => {
         <h1
           className="inline-block text-dark dark:text-light
       font-bold w-full !text-6xl xl:!text-5xl text-right
-      lg:!text-6xl md:!text-5xl sm:!text-3xl animate-fade sm:mb-8 mb-8"
+      lg:!text-6xl md:!text-5xl sm:!text-3xl animate-fade sm:mb-8 mb-16"
         >
           My works
         </h1>
-        <div className="grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        <div className="grid grid-cols-3 lg:grid-cols-2 sm:!grid-cols-1 gap-4">
           {projects.map((el) => {
             return (
-              <div key={el.id}>
-                <span>{el.title}</span>
-              </div>
+              <a
+                target="_blank"
+                href={el.link}
+                className="cursor-pointer select-none overflow-hidden group relative flex items-center justify-center"
+                key={el.id}
+              >
+                <div
+                  style={{ background: "rgba(0, 0, 0, 0.6)" }}
+                  className="absolute w-full h-full opacity-0
+                  grid place-items-center
+                  group-hover:opacity-100 z-50 ease-in text-white duration-300 transition-opacity"
+                >
+                  <div className="flex flex-col gap-2 items-center">
+                    <h3 className="font-medium text-2xl tracking-wide flex gap-2 items-center">
+                      {el.title}
+                    </h3>
+                  </div>
+                </div>
+                <div className="w-full lg:h-full h-72">
+                  <img
+                    className="w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out"
+                    src={el.img}
+                  />
+                </div>
+              </a>
             );
           })}
         </div>
