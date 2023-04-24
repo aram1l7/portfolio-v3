763f96236ff145313a807f355a6a0963ea559038
aram1l7 <arammartirosyann16@gmail.com>
2023-04-24 11:55:47 +0400

    Footer padding fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base ">
-      <Layout className="py-8 flex justify-between items-center xl:!p-12 md:!p-4 xs:flex-col xs:gap-3">
+      <Layout className="!py-8 flex justify-between items-center md:!pt-4 xl:!px-12 md:!p-4 xs:flex-col xs:gap-3">
         <span className="select-none flex items-center">
           &copy; {new Date().getFullYear()} by
           &#x1D49C;&#x1D4C7;&#x1D4B6;&#x1D4C2;
