d9085d1720ffc5e2bcafb36eaaa7b65b36b7db23
aram1l7 <arammartirosyann16@gmail.com>
2025-02-01 02:02:42 +0400

    Some updates

--- src/mocks/job-history.js
+++ src/mocks/job-history.js
@@ -3,7 +3,7 @@ export const jobHistory = [
     id: 6,
     position: "Full Stack Developer",
     company: "Grailpay",
-    time: "12/2023 - Present",
+    time: "03/2023 - Present",
     type: "Contract",
     location: "Remote",
     description: `
@@ -12,15 +12,16 @@ export const jobHistory = [
       <li>Built an SDK with TypeScript and Express to simplify client integration, boosting retention rates.</li>
       <li>Led end-to-end testing implementation with Jest and Cypress, achieving 95% test coverage.</li>
       <li>Enhanced internal QA tools, improving developer experience (DX) across teams.</li>
+      <li>Used <b class="text-primary dark:text-primaryDark">ChakraUI</b> for developing custom reusable components.</li>
     </ul>
     `,
   },
   {
-    id: 4,
+    id: 5,
     position: "Full Stack Developer",
-    company: "Itsoft",
-    time: "11/2022 - 12/2023",
-    type: "Contract",
+    company: "Bigbi",
+    time: "08/2021 - 03/2023",
+    type: "Full time",
     location: "Remote",
     description: `
       <ul class="list-disc">
@@ -32,27 +33,10 @@ export const jobHistory = [
       `,
   },
   {
-    id: 5,
+    id: 4,
     position: "Frontend Developer",
-    company: "Upwork",
-    time: "11/2022 - 06/2023",
-    type: "Contract",
-    location: "Remote",
-    description: `
-    <ul class="list-disc">
-    <li>Developed a business phone system dashboard using Vanilla JS and Vite.</li>
-    <li>Built cross-browser adaptive landing pages with Tailwind CSS and <b class="text-primary dark:text-primaryDark">Next.js</b>.</li>
-    <li>Developed an online pool cleaning shop with Next.js with Contentful CMS integration.</li>
-    <li>Designed and built a multi-page blog with React.js and Strapi CMS.</li>
-  </ul>
-
-    `,
-  },
-  {
-    id: 3,
-    position: "Full Stack Developer",
     company: "Cretrix",
-    time: "07/2021 - 11/2022",
+    time: "06/2020 - 08/2021",
     type: "Full time",
     location: "Yerevan, Armenia",
     description: `
@@ -62,16 +46,31 @@ export const jobHistory = [
       <li>Designed and implemented a design system for a full-stack blog, boosting development efficiency.</li>
       <li>Developed and maintained a content management system (CMS) with React and Redux, enhancing user engagement through an intuitive, scalable interface.</li>
   </ul>
-
+    `,
+  },
+  {
+    id: 3,
+    position: "Frontend Developer (Contract)",
+    company: "Upwork",
+    time: "03/2021 - 07/2021",
+    type: "Contract",
+    location: "Remote",
+    description: `
+    <ul class="list-disc">
+    <li>Developed a business phone system dashboard using Vanilla JS and Vite.</li>
+    <li>Built cross-browser adaptive landing pages with Tailwind CSS and <b class="text-primary dark:text-primaryDark">Next.js</b>.</li>
+    <li>Developed an online pool cleaning shop with Next.js and Contentful CMS integration.</li>
+    <li>Designed and built a multi-page blog with React.js and Strapi CMS.</li>
+  </ul>
     `,
   },
   {
     id: 2,
     position: "Frontend Developer",
     company: "Blowsys",
-    time: "06/2022 - 10/2022",
+    time: "03/2020 - 06/2020",
     type: "Contract",
-    location: "Yerevan, Armenia",
+    location: "Dubai, DU",
     description: `
     <ul class="list-disc">
   <li>Designed and implemented the company website using React.js and Next.js, optimizing performance and server-side rendering for improved load times.</li>
@@ -79,22 +78,20 @@ export const jobHistory = [
   <li>Integrated file upload functionality using Node.js and Nodemailer.</li>
   <li>Enhanced SEO by implementing meta tags, robots.txt, and structured data, resulting in higher search engine rankings and increased web traffic.</li>
 </ul>
-
     `,
   },
   {
     id: 1,
     position: "Frontend Developer",
-    company: "Evistep Enterprise",
-    time: "10/2020 - 07/2021",
+    company: "Evistep",
+    time: "10/2019 - 03/2020",
     location: "Yerevan, Armenia",
     type: "Full time",
     description: `
     <ul class="list-disc">
-  <li>Designed and developed a responsive landing page using <b class="text-primary dark:text-primaryDark">ES6</b>, Bootstrap and SCSS, improving conversion rates by 35%.</li>
+  <li>Designed a responsive landing page, improving conversion rates by 35%.</li>
   <li>Enhanced dashboard accessibility, leading to an 80% increase in mobile usability.</li>
 </ul>
-
     `,
   },
 ];
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,18 +4,14 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/theme/ThemeProvider";
 import Footer from "@/components/Footer";
 
-
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      {/* <main
-        className={`bg-light dark:bg-dark w-full min-h-[80vh] h-full`}
-      >
+      <main className={`bg-light dark:bg-dark w-full min-h-[80vh] h-full`}>
         <NavBar />
         <Component {...pageProps} />
         <Footer />
-      </main> */}
-      <h1>temporary unavailable</h1>
+      </main>
     </ThemeProvider>
   );
 }
