4a153ed1c14877c2865cf75e3ec39499960fa2e7
aram1l7 <arammartirosyann16@gmail.com>
2025-02-19 00:18:58 +0400

    Finish updates

---
+++ src/locales/index.js
@@ -0,0 +1,72 @@
+export const dictionary = {
+  en: {
+    name: "Aram Martirosyan",
+    role: "Senior Full Stack Javascript Developer",
+    info: `I'm a Senior Full Stack Javascript Developer with over 5 years of
+            experience, specializing in React (5+ years), React Native and Node.js (2+ years).
+            I've worked in fast-paced pre-seed startups as well as mid-sized international
+            companies across industries like blockchain, healthcare, artifical intelligence and
+            fintech. Currently open to opportunities in frontend, full stack,
+            and backend roles.`,
+    aboutMe: "About me",
+    contactMe: "Contact me",
+    contactHtml: `To discuss a project, you can drop me an
+              <a
+                class="text-blue-500 hover:underline transition-all ease-in duration-200"
+                href="mailto:aram18m@gmail.com"
+              >
+                
+                email
+              </a>
+              or message me on
+              <a
+                href="https://t.me/arammartirosyaan"
+                class="text-blue-500 hover:underline transition-all ease-in duration-200"
+                target="_blank"
+              >
+                Telegram 
+              </a>
+              or
+              <a
+                href="https://linkedin.com/in/arammartirosyann"
+                class="text-blue-500 hover:underline transition-all ease-in duration-200"
+                target="_blank"
+              >
+                Linkedin
+              </a>`,
+    footerText: "Aram Martirosyan. All rights reserved.",
+  },
+  ru: {
+    name: "Арам Мартиросян",
+    role: "Full Stack Javascript разработчик",
+    info: `Я Full Stack Javascript разработчик с опытом более 5 лет, фокусирован на React(5+ лет), 
+    React Native и Node.js(2+ года). Работал в развивающихся стартапах, а также в международных компаниях среднего размера. Работал в разных сферах - блокчейн, финтех, медтех, ИИ. В нынешнее время открыт для работы на следующих позициях: Frontend, Backend(Node.js), Full Stack`,
+    aboutMe: "Обо мне",
+    contactMe: "Связаться со мной",
+    contactHtml: `Чтобы обсудить проект можете написать сообщение на
+              <a
+                class="text-blue-500 hover:underline transition-all ease-in duration-200"
+                href="mailto:aram18m@gmail.com"
+              >
+                
+                почту
+              </a>
+              либо написать в
+              <a
+                href="https://t.me/arammartirosyaan"
+                class="text-blue-500 hover:underline transition-all ease-in duration-200"
+                target="_blank"
+              >
+                Телеграм 
+              </a>
+              или в
+              <a
+                href="https://linkedin.com/in/arammartirosyann"
+                class="text-blue-500 hover:underline transition-all ease-in duration-200"
+                target="_blank"
+              >
+                Linkedin
+              </a>`,
+    footerText: "Арам Мартиросян. Все права защищены.",
+  },
+};
